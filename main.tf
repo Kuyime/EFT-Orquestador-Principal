1233cd7960bf08446c5d6888b9b4e0019eeb31bd
Kuyime <enzo.soto15987@gmail.com>
2026-07-14 15:27:08 -0400

    style: formateo automático del código con terraform fmt

--- main.tf
+++ main.tf
@@ -9,14 +9,14 @@ data "aws_iam_role" "lab_role" {
 
 # 1. Llamada a TU Módulo de Redes
 module "redes" {
-  source = "git::https://github.com/Kuyime/EFT-Modulo-Redes.git?ref=v1.0.0"
+  source       = "git::https://github.com/Kuyime/EFT-Modulo-Redes.git?ref=v1.0.0"
   lab_role_arn = data.aws_iam_role.lab_role.arn
 }
 
 # 2. Llamada a TU Módulo de Cómputo
 module "computo" {
   source = "git::https://github.com/Kuyime/EFT-Modulo-Computo.git?ref=v1.0.0"
-  
+
   # Y aquí mantenemos la corrección que hicimos antes
   subnet_id         = module.redes.public_subnet_1_id
   security_group_id = module.redes.id_sg_ssh
